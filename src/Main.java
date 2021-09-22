edb39ea26cc334b47c0c5fe1b5555210e5492376
Pascal Haas <marketing54320@gmail.com>
2021-09-22 16:38:22 +0200

    Dames Depart v2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -6,13 +8,31 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Start methode appellée");
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("damier.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 800);
-        stage.setResizable(false);
-        stage.setTitle("Jeu de Dames");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Jeu de Dames");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+
+        // Stage fenetreDeux = new Stage();
+        // fenetreDeux.setTitle("Fenetre deux");
+        // fenetreDeux.setWidth(300);
+        // fenetreDeux.setHeight(300);
+        // fenetreDeux.show();
+
+        // // Lambda expression
+        // new Thread(() -> {
+        // try {
+        // Thread.sleep(5000);
+        // Platform.runLater(() -> fenetreDeux.hide());
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // }
+        // }).start();
 
     }
 
@@ -20,4 +40,13 @@ public class Main extends Application {
         launch();
 
     }
+
+    /**
+     * Methode appellée avant start !
+     */
+    @Override
+    public void init() {
+        System.out.println("Init methode appellée ");
+    }
+
 }
